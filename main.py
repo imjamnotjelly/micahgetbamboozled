4d82757b9a9ad36fea1445b7213d37670e89f89e
imjamnotjelly <jonahmitre@gmail.com>
2022-12-29 21:00:41 -0500

    various optimizations for exe compile

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import os
 from selenium import webdriver
 from time import sleep
 from time import strftime
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-print("""               /$$                     /$$                             /$$     /$$                               /$$                                     /$$                 /$$
+print("""               /$$                  cm   /$$                             /$$     /$$                               /$$                                     /$$                 /$$
               |__/                    | $$                            | $$    | $$                              | $$                                    | $$                | $$
  /$$$$$$/$$$$  /$$  /$$$$$$$  /$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$  | $$$$$$$   /$$$$$$  /$$$$$$/$$$$ | $$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$| $$  /$$$$$$   /$$$$$$$
 | $$_  $$_  $$| $$ /$$_____/ |____  $$| $$__  $$ /$$__  $$ /$$__  $$|_  $$_/  | $$__  $$ |____  $$| $$_  $$_  $$| $$__  $$ /$$__  $$ /$$__  $$|____ /$$/| $$ /$$__  $$ /$$__  $$
@@ -19,18 +23,22 @@ print("""               /$$                     /$$                             
                                                 |  $$$$$$/                                                                                                                      
                                                  \______/                                                                                                                       \n""")
 
+os.system("")
 colors = {
-    "red":"\033[31m",
-    "green":"\033[32m",
-    "yellow":"\033[33m",
-    "white":"\033[39m"
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "white": "\033[39m"
 }
 
-def tprint(data, color = "white"):
+
+def tprint(data, color="white"):
     print(colors[color] + f"[{strftime('%H:%M:%S')}] {data}" + "\033[0m")
 
-def tinput(data, color = "white"):
+
+def tinput(data, color="white"):
     return input(colors[color] + f"[{strftime('%H:%M:%S')}] {data}" + "\033[0m")
+
 
 class untilvis:
     def __init__(self, by, content):
@@ -42,31 +50,24 @@ class untilvis:
     def getmulti(self):
         return wait.until(EC.presence_of_all_elements_located(self.elem))
 
+
 def eleexists(id, name):
-    try:
-        exists = driver.find_element(id, name)
-    except NoSuchElementException:
-        return False
+    for i in range(10):
+        try:
+            exists = driver.find_element(id, name)
+        except NoSuchElementException:
+            sleep(0.1)
+        else:
+            return True
     else:
-        return True
+        return False
 
 
 tprint("Program started", "green")
 tprint("WARNING: DO NOT SELECT ANY GAME ELEMENTS AMID THE AUTOMATION PROCESS! THIS WILL TERMINATE THE PROGRAM!", "red")
-
-while True:
-    PATH = tinput(
-        r"Enter the location of your Chrome WebDriver, or enter skip if it is already stored in your enviromental variables/system PATH: ", "yellow")
-    try:
-        if PATH.lower() == "skip":
-            driver = webdriver.Chrome()
-        else:
-            driver = webdriver.Chrome(PATH)
-    except Exception as error:
-        tprint(f"Error: {error}", "red")
-        tprint("The location of your Chrome WebDriver is incorrect or not present in your system PATH.", "red")
-    else:
-        break
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 while True:
     uemail = tinput("Enter your Google account email that corresponds to your blooket account:  ", "yellow")
@@ -75,7 +76,6 @@ while True:
     else:
         break
 
-
 upassword = tinput("Enter your Google account password:  ", "yellow")
 gameamount = int(tinput("How many times would you like to run the gamemode?: ", "yellow"))
 tprint("Starting automation", "green")
@@ -83,31 +83,27 @@ tprint("Starting automation", "green")
 wait = WebDriverWait(driver, 25)
 driver.set_page_load_timeout(30)
 
-
 try:
-
-
 
     driver.get("https://id.blooket.com/login")
     tprint("Opening login link")
-    glogin = driver.find_element(By.CLASS_NAME, "arts__googleButton___1rop5-camelCase")
+    glogin = untilvis(By.CLASS_NAME, "arts__googleButton___1rop5-camelCase").get()
     glogin.click()
     sleep(2)
     cwindows = driver.window_handles
     driver.switch_to.window(cwindows[1])
     tprint("Changing window focus to popup")
 
-
-    ginp = untilvis(By.ID, "identifierId")
-    ginp.get().send_keys(uemail)
+    ginp = untilvis(By.ID, "identifierId").get()
+    ginp.send_keys(uemail)
     tprint("Entering email")
-    ginp.get().send_keys(Keys.ENTER)
+    ginp.send_keys(Keys.ENTER)
     sleep(2)
 
-    ginp = untilvis(By.CLASS_NAME, "whsOnd")
+    ginp = untilvis(By.CLASS_NAME, "whsOnd").get()
     tprint("Entering password")
-    ginp.get().send_keys(upassword)
-    ginp.get().send_keys(Keys.ENTER)
+    ginp.send_keys(upassword)
+    ginp.send_keys(Keys.ENTER)
     tprint("Changing window focus to primary window")
     driver.switch_to.window(cwindows[0])
 
@@ -119,60 +115,64 @@ try:
             driver.get("https://play.blooket.com/solo?id=63741c005b59391c7a779d15")
             sleep(3)
             tprint("Selecting correct gamemode")
-            fbutton = driver.find_element(By.XPATH, """//img[@alt="Factory"]""")
+            fbutton = untilvis(By.XPATH, """//img[@alt="Factory"]""").get()
             fbutton.click()
 
-
             tprint("Inputting correct information")
-            timeinput = driver.find_element(By.XPATH, """//input[@placeholder="Time"]""")
+            timeinput = untilvis(By.XPATH, """//input[@placeholder="Time"]""").get()
             timeinput.clear()
-            timeinput.send_keys("30")
+            timeinput.send_keys("35")
 
             sbutton = driver.find_element(By.CLASS_NAME, "styles__front___vcvuy-camelCase")
             sbutton.click()
             sleep(1)
-            stext = driver.find_element(By.CLASS_NAME, "styles__button___3LSjA-camelCase")
+            stext = untilvis(By.CLASS_NAME, "styles__button___3LSjA-camelCase").get()
             stext.click()
             sleep(1)
-            tpopup = driver.find_elements(By.CLASS_NAME, "styles__front___vcvuy-camelCase")[1]
+            tpopup = untilvis(By.CLASS_NAME, "styles__front___vcvuy-camelCase").getmulti()[1]
             tpopup.click()
             sleep(1)
 
-            tprint(f"Started game #{g+1}", "green")
+            tprint(f"Started game #{g + 1}", "green")
+
+            up_remind_amount = 0
+            button = untilvis(By.CLASS_NAME, "styles__answerContainer____unYj-camelCase")
+            background = untilvis(By.CLASS_NAME, "styles__feedbackContainer___2EWRn-camelCase")
+            skip = untilvis(By.CLASS_NAME, "styles__skipButton___3Ppa_-camelCase")
+            blook = untilvis(By.CLASS_NAME, "styles__blookChoice___1kAAj-camelCase")
             while True:
                 for i in range(3):
-                    tprint(f"Answering question {i+1}/3")
-                    button = untilvis(By.CLASS_NAME, "styles__answerContainer____unYj-camelCase")
+                    tprint(f"Answering question {i + 1}/3")
                     button.get().click()
-                    sleep(2)
-                    tprint("Checking for upgrade reminder")
-                    if eleexists(By.CLASS_NAME, "styles__remindButton___1gTTO-camelCase"):
-                        tprint("Reminder found")
-                        tprint("Negating upgrade reminder")
-                        reminder = driver.find_element(By.CLASS_NAME, "styles__remindButton___1gTTO-camelCase")
-                        reminder.click()
                     sleep(1.5)
-                    background = untilvis(By.CLASS_NAME, "styles__feedbackContainer___2EWRn-camelCase")
+                    if up_remind_amount < 3:
+                        tprint("Checking for upgrade reminder")
+                        if eleexists(By.CLASS_NAME, "styles__remindButton___1gTTO-camelCase"):
+                            tprint("Reminder found")
+                            tprint("Negating upgrade reminder")
+                            reminder = driver.find_element(By.CLASS_NAME, "styles__remindButton___1gTTO-camelCase")
+                            reminder.click()
+                            up_remind_amount += 1
+                            print(f"Eliminated reminder {up_remind_amount}/3")
+                    sleep(1.5)
                     background.get().click()
                     sleep(1)
                 if eleexists(By.CLASS_NAME, "styles__skipButton___3Ppa_-camelCase"):
                     tprint("Skipping upgrade")
-                    skip = driver.find_element(By.CLASS_NAME, "styles__skipButton___3Ppa_-camelCase")
-                    skip.click()
+                    skip.get().click()
 
                 else:
                     tprint("Selecting random blook")
-                    blook = untilvis(By.CLASS_NAME, "styles__blookChoice___1kAAj-camelCase")
                     blook.get().click()
 
                 sleep(1.5)
         except:
             tprint("Game finished", "green")
             tprint("Selecting random token multiplier")
-            tokenm = untilvis(By.CLASS_NAME, "styles__prizeSet___35U-m-camelCase")
-            tokenm.get().click()
-            continuebtn = untilvis(By.CLASS_NAME, "styles__button___22rMT-camelCase")
-            continuebtn.get().click()
+            tokenm = untilvis(By.CLASS_NAME, "styles__prizeSet___35U-m-camelCase").get()
+            tokenm.click()
+            continuebtn = untilvis(By.CLASS_NAME, "styles__button___22rMT-camelCase").get()
+            continuebtn.click()
             tprint("Proceeding to next game")
     tprint("Automation complete", "green")
     for i in range(3, 0, -1):
