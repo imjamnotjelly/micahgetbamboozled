5fa112bae31a317de3415f9d1fb842891f0790ca
imjamnotjelly <81252683+imjamnotjelly@users.noreply.github.com>
2023-01-25 11:32:15 -0500

    revert test.py event listener method

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -2,14 +2,13 @@ let username
 let password
 let headless
 
-function print_inputs(){
+function print_inputs() {
 	username = document.getElementById("u_input").value
 	password = document.getElementById("p_input").value
 	headless = document.getElementById("headless_cb").checked
-	console.log(`Username: ${username}`)
-	console.log(`Password: ${password}`)
-	console.log(`Headless: ${headless}`)
-	eel.wait_switch()
+	console.log(`Email: ${username}`)
+	console.log(`Password: ${"*".repeat(password.length)}`)
+	console.log(`Headless-mode: ${headless}`)
 }
 
 eel.expose(return_inputs, "return_inputs")
--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,13 +1,6 @@
 import eel
 
 eel.init("../../src")
-
-form_wait = True
-
-@eel.expose
-def wait_switch():
-    form_wait = not form_wait
-
 eel.start("frontend/login/login.html", block=False)
 print("Eel initialized!")
 
@@ -15,18 +8,13 @@ username = ""
 password = ""
 headless = False
 
-while form_wait:
-    pass
-
-username, password, headless = eel.return_inputs()()
+while not username:
+    username, password, headless = eel.return_inputs()()
 eel.close_window()
 
 print(f"""
-Username: {username}
-Password: {password}
+Email: {username}
+Password: {"*"*len(password)}
 Headless: {headless}
 this is in python btw!!!
 """)
-eel.start("frontend/platform_select/platform_select.html", block=False)
-
-
